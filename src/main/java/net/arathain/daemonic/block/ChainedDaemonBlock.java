c184b16958adea09652c3dadb78547ef12e0fcb9
Arathain <76701473+Arathain@users.noreply.github.com>
2021-02-16 14:35:08 +0100

    Add chainmail burn functionality, possession via block break attempt

--- src/main/java/net/arathain/daemonic/block/ChainedDaemonBlock.java
+++ src/main/java/net/arathain/daemonic/block/ChainedDaemonBlock.java
@@ -1,6 +1,20 @@
 package net.arathain.daemonic.block;
 
+import io.github.apace100.origins.component.OriginComponent;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.origin.OriginLayer;
+import io.github.apace100.origins.origin.OriginLayers;
+import io.github.apace100.origins.origin.OriginRegistry;
+import io.github.apace100.origins.registry.ModComponents;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 
 public class ChainedDaemonBlock extends Block {
 
@@ -8,4 +22,18 @@ public class ChainedDaemonBlock extends Block {
         super(settings);
     }
 
+    @Override
+    public void onBreak(World world, BlockPos blockPos, BlockState state, PlayerEntity playerEntity) {
+
+        ItemStack itemStack = playerEntity.getMainHandStack();
+        OriginComponent oc = ModComponents.ORIGIN.get(playerEntity);
+        OriginLayer layer = OriginLayers.getLayer(new Identifier("origins", "origin"));
+        Origin origin = OriginRegistry.get(new Identifier("daemonic", "cursed"));
+        if (itemStack.getEnchantments().contains(Enchantments.SILK_TOUCH)) {
+
+        } else {
+            oc.setOrigin(layer, origin);
+        }
+    }
 }
+
